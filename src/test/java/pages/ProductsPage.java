3ee061eaf573bd1cbead494a6a5b54ca7f6f3016
Лилия <liliaht8@gmail.com>
2026-04-10 17:40:13 +0300

    BaseTest, ProductTest

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -3,15 +3,21 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class ProductsPage {
+public class ProductsPage extends BasePage {
     WebDriver driver;
     private final By pageTitle = By.cssSelector("[data-test='title']");
+    private final By addToCartBtn = By.xpath("//*[text()='Add to cart']");
+
 
     public ProductsPage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
     public String getTitle() {
         return driver.findElement(pageTitle).getText();
+    }
+
+    public void addToCart() {
+        driver.findElement(addToCartBtn).click();
     }
 }
